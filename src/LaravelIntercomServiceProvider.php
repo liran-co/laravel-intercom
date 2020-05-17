86154e500059805ed1977a5ba89655ad1bdb02bd
Liran Cohen <l@liran.co>
2020-05-17 19:57:48 -0400

    Update LaravelIntercomServiceProvider.php

--- src/LaravelIntercomServiceProvider.php
+++ src/LaravelIntercomServiceProvider.php
@@ -8,27 +8,17 @@ use Illuminate\Contracts\Container\Container;
 
 class LaravelIntercomServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/intercom.php' => config_path('intercom.php'),
-            ], 'config');
-        }
+        $this->publishes([
+            __DIR__.'/../config/intercom.php' => config_path('intercom.php'),
+        ]);
     }
 
-    /**
-     * Register the application services.
-     */
     public function register()
     {
-        // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/intercom.php', 'intercom');
-
-        // Register the main class to use with the facade
+        
         $this->app->singleton('intercom', function (Container $app) {
 	        
 	        $client = new IntercomClient($app['config']->get('intercom.access_token'));
